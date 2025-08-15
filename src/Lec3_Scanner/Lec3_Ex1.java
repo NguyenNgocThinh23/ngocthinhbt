65b1068d9c17c0bb4f6bba52963da6bdea5a1eb5
Nguyễn Ngọc Thịnh <thinhnguyenngoc6@gmail.com>
2025-08-15 16:55:25 +0700

    Delete src/Lec3_Scanner/Lec3_Ex1.java

--- src/Lec3_Scanner/Lec3_Ex1.java
+++
@@ -1,7 +0,0 @@
-package Lec3_Scanner;
-public class Lec3_Ex1 {
-    public static void main(String[] args) {
-        int number = 42;
-        System.out.println("The answer is " + number);
-    }
-}
